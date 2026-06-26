140f5c55b0cd44846a0fc14296c4d64d3be6a379
arashfar <saeedarashfar1371@gmail.com>
2026-06-26 21:29:54 +0330

    add-manual-serilization

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	//"time"
 )
 
@@ -19,18 +21,30 @@ type Task struct {
 	ID    int
 	Title string
 	//DueDate  time.Time
-	DueDate  string
-	Category string
-	IsDone   bool
-	UserID   int
+	DueDate    string
+	CategoryID int
+	IsDone     bool
+	UserID     int
+}
+
+type Category struct {
+	ID     int
+	Title  string
+	Color  string
+	UserID int
 }
 
 // global
 var taskStorage []Task
 var userStorage []User
+var categoryStorage []Category
 var authenticatedUser *User
 
+const userStoragePath string = "users.txt"
+
 func main() {
+	// load data
+	leadUserStorageFromFile()
 	fmt.Println("Hello to TODO app")
 
 	command := flag.String("command", "no-command", "command to run")
@@ -45,7 +59,6 @@ func main() {
 	}
 
 	fmt.Println("userrStorage: %+v\n")
-
 }
 
 func runCommand(command string) {
@@ -63,6 +76,8 @@ func runCommand(command string) {
 		createCategory()
 	case "register-user":
 		registerUser()
+	case "list-task":
+		listTask()
 	case "login":
 		login()
 	case "exit":
@@ -77,6 +92,64 @@ func (u User) Print() {
 	fmt.Println("User: ", u.ID, u.Email, u.Name)
 }
 
+func leadUserStorageFromFile() {
+	file, err := os.Open(userStoragePath)
+	if err != nil {
+		fmt.Println("can't open the file.", err)
+	} else {
+		var data = make([]byte, 1024)
+		_, oErr := file.Read(data)
+		if oErr != nil {
+			fmt.Println("can't read from the file", oErr)
+		} else {
+			dataStr := string(data)
+			dataStr = strings.Trim(dataStr, "\n")
+			dataList := strings.Split(dataStr, "\n")
+			for _, item := range dataList {
+				if item == "" {
+					continue
+				}
+				// fmt.Printf("[%d (len:%d)]: %s\n", index, len(item), item)
+				itemList := strings.Split(item, ",")
+
+				user := User{}
+				isValidRecord := true
+				for _, field := range itemList {
+					values := strings.Split(field, ": ")
+					if len(values) != 2 {
+						fmt.Println("record is not vaild")
+						isValidRecord = false
+						break
+					}
+					fieldName := strings.TrimSpace(values[0])
+					fieldValue := strings.TrimSpace(values[1])
+
+					switch fieldName {
+					case "id":
+						id, err := strconv.Atoi(fieldValue)
+						if err != nil {
+							fmt.Println("strconv failed.", err)
+							return
+						}
+						user.ID = id
+					case "name":
+						user.Name = fieldValue
+					case "email":
+						user.Email = fieldValue
+					case "password":
+						user.Password = fieldValue
+					}
+				}
+				if isValidRecord {
+					userStorage = append(userStorage, user)
+				}
+			}
+			file.Close()
+			fmt.Println(userStorage)
+		}
+	}
+}
+
 func createTask() {
 	scanner := bufio.NewScanner(os.Stdin)
 	var title, duedate, category string
@@ -85,23 +158,50 @@ func createTask() {
 	scanner.Scan()
 	title = scanner.Text()
 
-	fmt.Println("please enter the task category")
+	fmt.Println("please enter the task category Id")
 	scanner.Scan()
 	category = scanner.Text()
+
+	categoryID, err := strconv.Atoi(category)
+	if err != nil {
+		fmt.Printf("category id is not valid integer. %v\n", err)
+		return
+	}
+
+	isFound := false
+	for _, c := range categoryStorage {
+		if c.UserID == authenticatedUser.ID && c.ID == categoryID {
+			isFound = true
+
+			break
+		}
+	}
+	if !isFound {
+		fmt.Print("category id is not found")
+		return
+	}
 
 	fmt.Println("please enter the task due date")
 	scanner.Scan()
 	duedate = scanner.Text()
 
 	task := Task{
-		ID:       len(taskStorage) + 1,
-		Title:    title,
-		DueDate:  duedate,
-		Category: category,
-		IsDone:   false,
-		UserID:   authenticatedUser.ID,
+		ID:         len(taskStorage) + 1,
+		Title:      title,
+		DueDate:    duedate,
+		CategoryID: categoryID,
+		IsDone:     false,
+		UserID:     authenticatedUser.ID,
 	}
 	taskStorage = append(taskStorage, task)
+}
+
+func listTask() {
+	for _, task := range taskStorage {
+		if task.UserID == authenticatedUser.ID {
+			fmt.Println(task)
+		}
+	}
 }
 
 func createCategory() {
@@ -116,7 +216,15 @@ func createCategory() {
 	scanner.Scan()
 	color = scanner.Text()
 
-	fmt.Println("title: ", title, "-color:", color)
+	// fmt.Println("title: ", title, "-color:", color)
+
+	category := Category{
+		ID:     len(categoryStorage) + 1,
+		Title:  title,
+		Color:  color,
+		UserID: authenticatedUser.ID,
+	}
+	categoryStorage = append(categoryStorage, category)
 }
 
 func registerUser() {
@@ -147,6 +255,16 @@ func registerUser() {
 	}
 
 	userStorage = append(userStorage, user)
+	file, err := os.OpenFile(userStoragePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0777)
+	if err != nil {
+		fmt.Println("can't open the user.txt file", err)
+		return
+	}
+	data := fmt.Sprintf("id: %d, name: %s, email: %s, password: %s\n", user.ID, user.Name, user.Email, user.Password)
+	b := []byte(data)
+	file.Write(b)
+
+	file.Close()
 }
 
 func login() {
